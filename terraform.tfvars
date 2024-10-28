90621c15d0dba471b40089578d8c097808759a0c
syudenky-admin <zhuchuanhui@hotmail.com>
2024-10-29 00:52:30 +0900

    Initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+compartment_id   = "ocid1.tenancy.oc1..aaaaaaaaidabeh4bww766raesn274xyaormyuplogucdzg3fum4n43eyacja"
+region           = "ap-osaka-1"
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "compartment_id" {
+  description = "ocid1.tenancy.oc1..aaaaaaaaidabeh4bww766raesn274xyaormyuplogucdzg3fum4n43eyacja"
+  type        = string
+}
+variable "region" {
+  description = "region where you have OCI tenancy"
+  type        = string
+  default     = "ap-osaka-1"
+}
+
